1044a29f4a2598a76125db0ce2d8947bf13f2804
Giovani <gio.gimenez@gmail.com>
2021-02-15 19:17:26 -0300

    Actualizacion 1.1

--- app/controllers/estadisticas_controller.rb
+++ app/controllers/estadisticas_controller.rb
@@ -1,8 +1,15 @@
 class EstadisticasController < ApplicationController
     def index 
     @temporal = [1, 2, 3, 4, 5, 6, 7 ]
-    @user = Tarea.group(:user_id)
-        
+    query="select user_id  from tareas  where estado=true group by user_id"
+
+    @user = ActiveRecord::Base.connection.execute(query).values
+    
+    query1="select user_id  from tareas  where estado=false group by user_id"
+    
+    @user1 = ActiveRecord::Base.connection.execute(query1).values
+
+    
     
     end
 end
